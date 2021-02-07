9231c9cfb4824b0f604ee5fa9d06c708ee638451
aakerley <aakerley@tamu.edu>
2021-02-06 18:12:11 -0800

    Some clicker and delay updates

--- bot_builder/delay.py
+++ bot_builder/delay.py
@@ -2,10 +2,8 @@ import random
 import time
 
 class delay:
-    def __init__(self, chance, low, high):
-        self.chance = chance
-        self.low = low
-        self.high = high
+    def __init__(self):
+	    print("Hello peasants")
 
     def fixed(self, fixed_duration): # Waits for a fixed amount of time
         time.sleep(fixed_duration)
@@ -20,27 +18,31 @@ class delay:
     def fixed_chance(self, fixed_duration, chance):
         my_rand = random.randint(1,chance)
         if my_rand == 1:
+            print("Hit chance")
             time.sleep(fixed_duration)
 
     def random_chance(self, random_duration, chance):
         my_rand = random.randint(1,chance)
         if my_rand == 1:
+            print("Hit chance")
             time.sleep(random.uniform(0, random_duration))
 
     def fixed_random_chance(self, fixed_duration, random_duration, chance):
         time.sleep(fixed_duration)
         my_rand = random.randint(1,chance)
         if my_rand == 1:
+            print("Hit chance")
             time.sleep(random.uniform(0, random_duration))
 
     def fixed_chance_random_chance(self, fixed_duration, random_duration, chance):
         my_rand = random.randint(1,chance)
         if my_rand == 1:
+            print("Hit chance")
             time.sleep(fixed_duration)
             time.sleep(random.uniform(0, random_duration))
 
     def fcrc(self, fixed_duration, random_duration, chance): # Easier Name
-        fixed_chance_random_chance(fixed_duration, random_duration, chance)
+        self.fixed_chance_random_chance(fixed_duration, random_duration, chance)
 
     def frc(self, fixed_duration, random_duration, chance): # Easier Name
-        fixed_random_chance(fixed_duration, random_duration, chance)
+        self.fixed_random_chance(fixed_duration, random_duration, chance)
